82e8d155ac05c33a0637a553cc199f0d9de09e0b
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2021-06-25 16:01:21 +0600

    Update polynomial_regressiontest_delet.py

--- Electric Vehicle Range Prediction/Polynomial Regression/polynomial_regressiontest_delet.py
+++ Electric Vehicle Range Prediction/Polynomial Regression/polynomial_regressiontest_delet.py
@@ -16,8 +16,6 @@ from sklearn.model_selection import train_test_split
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-
-
 # Training the Polynomial Regression model on the whole dataset
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
