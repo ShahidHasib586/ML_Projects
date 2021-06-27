10bc1b5a4b760c735db9b2a89cf8fceebc966493
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2021-06-27 13:43:31 +0600

    Update Simple Lineaar regression analysis Regression.py

--- Electric Vehicle Range Prediction/Simple Linear Regression/Simple Lineaar regression analysis Regression.py
+++ Electric Vehicle Range Prediction/Simple Linear Regression/Simple Lineaar regression analysis Regression.py
@@ -11,8 +11,6 @@ dataset = pd.read_csv('M.csv')
 X = dataset.iloc[:, 13:-1].values
 y = dataset.iloc[:, -1].values
 
-
-
 from sklearn.model_selection import train_test_split
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
